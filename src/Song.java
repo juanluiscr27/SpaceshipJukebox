eae23571749ad98b56162769cb35221bccd2b531
Juan Luis <juanluiscr27@hotmail.com>
2022-12-27 17:44:17 -0500

    Song class implementation

---
+++ src/Song.java
@@ -0,0 +1,21 @@
+public class Song {
+    private final String title;
+    private final String artist;
+
+    public Song(String title, String artist) {
+        this.title = title;
+        this.artist = artist;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getArtist() {
+        return artist;
+    }
+
+    public void play() {
+        System.out.println("The song " + this.title + " from " + this.artist + " is now playing...");
+    }
+}
